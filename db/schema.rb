b6fd9ded1ce7a31ac4af69f7ad2bf8c393abecd5
India Kato <india.kato@gmail.com>
2021-02-10 14:28:26 -0800

    added host name to groups db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_200619) do
+ActiveRecord::Schema.define(version: 2021_02_10_200553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_08_200619) do
     t.string "group_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "host_name"
   end
 
   create_table "restaurants", force: :cascade do |t|
