0ab3cda69330626000e1816afee2adfd456927f5
India Kato <india.kato@gmail.com>
2021-02-12 11:28:00 -0800

    restaurants working FOR THE THIRD TIME

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,15 +5,9 @@ KEY = ENV["YELP_API_KEY"]
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
-    data = Restaurant.all
+    data = @restaurants
 
     render status: :ok, json: data
   end
-
-  def create
-    data = YelpApiWrapper.search("seattle")
-    render status: :ok, json: data
-  end
-
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -13,5 +13,17 @@ class Group < ApplicationRecord
     end
     return group_members
   end
+
+  # def self.get_restaurants(group_name)
+  #   group = Group.find_by(group_name)
+  #   group_restaurants = []
+  #   restaurants = Restaurant.all
+  #   restaurants.each do |restaurant|
+  #     if restaurant.group_id == group.id
+  #       group_restaurants << restaurant
+  #     end
+  #   end
+  #   return group_restaurants
+  # end
   
 end
